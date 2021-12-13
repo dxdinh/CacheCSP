543133efc2cede436030730f0cde69999a803ee2
DX DX <5952219+dxdinh@users.noreply.github.com>
2021-12-13 14:37:10 -0800

    Create query_testspec.sql

---
+++ sql/query_testspec.sql
@@ -0,0 +1,7 @@
+# Query list of testspec in the system
+```
+Select  ad.Name,  ts.* from stats.testspec ts
+ JOIN Codelist.AnalyteDescription ad ON ts.Analyte = ad.Analyte and languageKey = 'en'
+ ```
+ 
+ 
